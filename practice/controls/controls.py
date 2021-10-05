e54d84af737e45912384ef682b28bdd975869ee0
Klaimmorz <artemus92@mail.ru>
2021-10-05 22:37:45 +0300

    Task [pr] controls done

--- practice/controls/controls.py
+++ practice/controls/controls.py
@@ -3,19 +3,45 @@ import math
 
 def transpose(a):
     """Транспозиция матрицы"""
-    pass
+    s = []
+    for i in range(len(a[0])):
+        s.append(list())
+        for j in range(len(a)):
+            s[i].append(a[j][i])
+    return s
 
 
 def read_lines(input, print):
     """Количество непустых строк во вводе до первой пустой"""
-    pass
+    i = 0
+    s = ''
+    while True:
+        s = input()
+        if s == '':
+            break
+        else:
+            i += 1
+    print(i)
 
 
 def min_prime(n):
     """Минимальный простой делитель числа"""
-    pass
-
+    if n % 2 == 0:
+        return 2
+    i = 3
+    while n % i != 0 and i * i <= n:
+        i += 2
+    if i * i <= n:
+        return i
+    return n
 
 def from_hexadecimal(s):
     """Перевод из шестнадцатеричной системы счисления"""
-    pass
+    s16 = "0123456789ABCDEF"
+    acc = 0
+    for a in s:
+        p = s16.find(a.upper())
+        if (p >= 16) | (p == -1):
+            return None
+        acc = acc * 16 + p
+    return acc
