588503f45a259c607c144bb9bf709da387d4a536
Klaimmorz <artemus92@mail.ru>
2021-10-05 22:39:04 +0300

    Task [pr] structures done

--- practice/structures/structures.py
+++ practice/structures/structures.py
@@ -1,23 +1,47 @@
 def kth(a, k):
     """k-е по величине значение в массиве"""
-    pass
+    a.sort()
+    return a[k-1]
 
 
 def head_tail(a, k):
     """Первые и последние k элементов в массиве"""
-    pass
+    return a[:k] + a[len(a)-k:]
 
 
 def filter_sort(a, c):
     """Отсортировать копию массива, удалив строки с символом c"""
-    pass
+    g = a
+    g.sort()
+    i = 0
+    flag = 0
+    while i < len(g):
+        print(i)
+        for j in g[i]:
+            if j == c:
+                flag += 1
+        if flag > 0:
+            g.remove(g[i])
+            flag = 0
+        else:
+            i += 1
+    return g
+
 
 
 def build_dict(keys, values):
     """Построить словарь по набору ключей и значений"""
-    pass
+    k = dict(zip(keys, values))
+    return k
 
 
 def compare_contents(a1, a2):
     """Проверка наборов элементов на совпадение"""
-    pass
+    for i in a1:
+        if a2.count(i) == 0:
+            return False
+    for i in a2:
+        if a1.count(i) == 0:
+            return False
+    return True
+
